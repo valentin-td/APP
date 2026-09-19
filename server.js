add5d506ee2ec70665aca32d7ec5665bde9faaaa
valentin-td <tardy.valentin04@gmail.com>
2026-09-19 22:37:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -552,7 +552,7 @@ async function analyserEmailAvecIA(sujet, texte) {
 
     try {
         const completion = await groq.chat.completions.create({
-            model: 'llama-3.1-70b-versatile',
+            model: 'llama-3.1-8b-instant',
             temperature: 0,
             response_format: { type: 'json_object' },
             messages: [
