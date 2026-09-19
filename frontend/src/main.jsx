85b717f2aa1efca284041fbf8e934c72eacfdb1f
valentin-td <tardy.valentin04@gmail.com>
2026-09-19 19:04:45 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,18 @@ import './index.css';
 // Import du module PWA
 import { registerSW } from 'virtual:pwa-register';
 
-// Enregistrement immédiat du Service Worker
-registerSW({ immediate: true });
+// Enregistrement forcé du Service Worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // Recharge la page si une nouvelle mise à jour du code est détectée
+    if (confirm('Nouvelle mise à jour disponible. Recharger ?')) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('✅ Le mode hors-ligne est installé et prêt à être utilisé !');
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
