dcfeb80ad87931bfc9e7eaac2de55e0653280b9a
valentin-td <tardy.valentin04@gmail.com>
2026-09-19 12:30:03 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Import du module hors-ligne (Service Worker)
+import { registerSW } from 'virtual:pwa-register';
+
+// Lancement immédiat de la mise en cache de l'application
+registerSW({ immediate: true });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
